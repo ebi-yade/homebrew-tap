fb60ea0eca76f965b2034a9600108477dad5ce77
市川恭佑 <github@mail.ebi-yade.com>
2024-10-27 20:00:58 +0900

    Update ecresolve

--- ecresolve.rb
+++ ecresolve.rb
@@ -1,27 +1,27 @@
 class Ecresolve < Formula
   desc 'Resolves AWS ECR images with prioritized tag-based lookup'
-  version '0.0.4'
+  version '0.0.5'
   homepage 'https://github.com/ebi-yade/ecresolve'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.4/ecresolve_0.0.4_darwin_arm64.tar.gz'
-      sha256 '8927be44e6ed261c2177b8965984eab4438315f660d6f439deb1d91710cb46bc'
+      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.5/ecresolve_0.0.5_darwin_arm64.tar.gz'
+      sha256 'd994b8f4e148875a61460b775597bba3e262f476bc4a81cb546d521e2a645732'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.4/ecresolve_0.0.4_darwin_amd64.tar.gz'
-      sha256 '22e33023a7d3e163707ca1a6f7d7d8ed3c6789b5e7f3ef16fdee424860474a60'
+      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.5/ecresolve_0.0.5_darwin_amd64.tar.gz'
+      sha256 '7ebde2c34215c63a88b0772226e4985ae05c1afbc00a445dfb4875e339492842'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.4/ecresolve_0.0.4_linux_arm64.tar.gz'
-      sha256 '07e95840097ee0498fd98c0940d403add08b4e1450d433ce570bd98cf8e4f280'
+      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.5/ecresolve_0.0.5_linux_arm64.tar.gz'
+      sha256 '623ebb80e97f2a39ca75a1b12d3f116045333a195e4c7dca8510bbf39b44012b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.4/ecresolve_0.0.4_linux_amd64.tar.gz'
-      sha256 '20dad90ff17671f7c1075b4e223709526912a9f9848618e1597092d495f7a8e5'
+      url 'https://github.com/ebi-yade/ecresolve/releases/download/v0.0.5/ecresolve_0.0.5_linux_amd64.tar.gz'
+      sha256 '65cf19f1c4be8b5ec5e59ab6ff31f1cc987748281c5ad55c608ff1dd6622ccc6'
     end
   end
 
